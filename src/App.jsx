ced0de4f2d8c2b62a54a04d55fe1adbc80b61172
siddiq <siddiqahmed882@gmail.com>
2024-12-19 21:05:15 +0500

    audio element added

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,11 @@ import FamilyTreeApp from './FamilyTree';
 import Wheel from './Wheel';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
-import { useEffect, useRef, useState } from 'react';
-import audiodata1 from './assets/audio1.mp3';
 
 function App() {
-  const audioElement = new Audio(audiodata1);
-  const hasUserInteractedWithPage = useRef(false);
-  const [music, setMusic] = useState(true);
-
-  useEffect(() => {
-    function detectClickAndPlay() {
-      if (hasUserInteractedWithPage.current) return;
-      hasUserInteractedWithPage.current = true;
-
-      if (audioElement.paused) {
-        audioElement.loop = true;
-        audioElement.play();
-        document.removeEventListener('click', detectClickAndPlay);
-      }
-    }
-
-    document.addEventListener('click', detectClickAndPlay);
-
-    return () => {
-      document.removeEventListener('click', detectClickAndPlay);
-    };
-  }, []);
-  
-  // Update audio playback based on `music` state
-  useEffect(() => {
-      if (music) {
-        // audioElement.current.loop = true;
-        if (audioElement.paused) {
-          audioElement.play().catch((error) => {
-            console.error("Error playing audio:", error);
-          });
-        }
-      } else {
-        console.log("first")
-        audioElement.pause();
-        // audioElement.current.currentTime = 0; // Optional: reset to the start
-      }
-  }, [music]);
-
   return (
     <Router>
-      <Navbar music={music} setMusic={setMusic} />
+      <Navbar />
       <Routes>
         <Route path='/' element={<FamilyTreeApp />} />
         <Route path='/wheel' element={<Wheel />} />
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,79 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import { axiosClient } from './axios-client';
 
-const Navbar = ({music, setMusic}) => {
+const Navbar = () => {
+  const [audioFile, setAudioFile] = useState(null);
+  const [audioPlaying, setAudioPlaying] = useState(false);
+
+  const abortController = useRef();
+  const audioElementRef = useRef(null);
+
+  useEffect(() => {
+    abortController.current = new AbortController();
+    const signal = abortController.current.signal;
+
+    axiosClient
+      .get('/songs/selected', { responseType: 'blob', signal })
+      .then((response) => {
+        const audioBlob = new Blob([response.data], { type: 'audio/mpeg' });
+        setAudioFile(URL.createObjectURL(audioBlob));
+      })
+      .catch((error) => {
+        console.error('Error fetching audio file:', error);
+      });
+
+    return () => {
+      abortController.current.abort
+        ? abortController.current.abort()
+        : abortController.current.abort();
+    };
+  }, []);
+
+  useEffect(() => {
+    function detectClickAndPlay() {
+      if (!audioElementRef.current) return;
+
+      if (audioPlaying) return;
+
+      if (audioElementRef.current.paused) {
+        audioElementRef.current.loop = true;
+        audioElementRef.current.play();
+        setAudioPlaying(true);
+        document.removeEventListener('click', detectClickAndPlay);
+      }
+    }
+
+    document.addEventListener('click', detectClickAndPlay);
+
+    return () => {
+      document.removeEventListener('click', detectClickAndPlay);
+    };
+  }, [audioPlaying, audioElementRef]);
+
+  function handleAudioButtonClick() {
+    if (!audioElementRef.current) return;
+
+    if (audioPlaying) {
+      audioElementRef.current.pause();
+    } else {
+      audioElementRef.current.play();
+    }
+  }
 
   return (
     <nav className='navbar navbar-light bg-light | app-navbar'>
       <div className='container-fluid d-flex justify-content-between'>
+        <audio
+          ref={audioElementRef}
+          autoPlay={true}
+          loop={true}
+          src={audioFile}
+          className='sr-only'
+          onPlay={() => setAudioPlaying(true)}
+          onPause={() => setAudioPlaying(false)}
+        />
+
         <div>
           <a className='navbar-brand' href='/'>
             Family Tree
@@ -33,7 +101,13 @@ const Navbar = ({music, setMusic}) => {
             </a>
           </li>
           <li>
-            <button className='btn btn-success' onClick={() => setMusic((prev) => !prev) }>{music ? "Pause" : "Play"}</button>
+            <button
+              className='btn btn-success'
+              disabled={!audioFile}
+              onClick={handleAudioButtonClick}
+            >
+              {audioPlaying ? 'Pause' : 'Play'}
+            </button>
           </li>
         </ul>
       </div>
